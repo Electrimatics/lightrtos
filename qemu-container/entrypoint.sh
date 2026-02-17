f105b771fac664c249d0cc8602b23191828b18bc
Henry C. Budris <henry.budris@gmail.com>
2026-02-16 20:36:59 -0500

    Removed netcat as dependency inside container

--- qemu-container/entrypoint.sh
+++ qemu-container/entrypoint.sh
@@ -2,13 +2,13 @@
 # START_TELNET, START_DEBUG, QEMU_RUN_ARGS populated by docker run command (using -e flag)
 
 if [ $START_TELNET -ne 0 ]; then
-    lxterminal --title "QEMU USART" -e "until kill -0 $(pidof qemu-system-avr); do : ; done; sleep 1; telnet localhost 10000;" &
+    lxterminal --title "QEMU USART" -e "until kill -0 $(pidof qemu-system-avr); do : ; done; telnet localhost 10000;" &
 fi
 
 if [ $START_DEBUG -ne 0 ]; then
     # TODO: Add ability to pass GDB file in
     QEMU_RUN_ARGS+="--gdb tcp::10001 -S"
-    lxterminal --title "QEMU DEBUG" -e "until nc -z localhost 10001; do sleep 0.1; done; \
+    lxterminal --title "QEMU DEBUG" -e "until kill -0 $(pidof qemu-system-avr); do : ; done; \
         gdb-multiarch -iex 'set remotetimeout unlimited' \
         -iex 'set architecture avr' \
         -iex 'symbol-file /firmware.img' \
